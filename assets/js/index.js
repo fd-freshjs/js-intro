900044db57408542eae3c501086a55ce84919c5d
Yevhenii <freshjs.fte.@gmail.com>
2022-01-26 16:31:12 +0200

    Add if example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,59 @@
+console.log("global");
 
-console.log('global');
-
-let glob = 'glob'
+let glob = "glob";
 
 function sayYourAge() {
   // definition
   const num = prompt("Введите ваш возраст");
 
-  console.log('outer func');
-
-  function func () {
-    let innerNum = 0;
-
-    console.log('inner func')
-    console.log(glob);
-    console.log(num);
-    console.log(innerNum);
-  }
-  func()
+  console.log("outer func");
 
   return num;
 }
 
-const result = sayYourAge();
+// const result = sayYourAge();
+
+// console.log(result);
+
+/////////////////////////////
+
+const a = prompt("Введите сторону a трапеции");
+const b = prompt("Введите сторону b трапеции");
+const ha = prompt("Введите высоту трегольника");
+
+
+
+
+
+function trapecArea(numa, numb, numh) {
+  let prepA = numa;
+  let prepB = numb;
+  let prepH = numh;
+
+  if (typeof numa !== 'number') {
+    prepA = Number(numa);
+  }
+
+  if (typeof numb !== 'number') {
+    prepB = Number(numb);
+  }
+
+  if (typeof numh !== 'number') {
+    prepH = Number(numh);
+  }
+
+  return ((prepA + prepB) / 2) * prepH;
+}
+
+
+
+
+
+
+
+const result = trapecArea(a, b, ha);
 
 console.log(result);
+
+// создать функцию для расчета площади трапеции,
+// принимать числа как параметры
