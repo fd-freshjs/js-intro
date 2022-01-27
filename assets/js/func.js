1c20315a90a398f00f7292cad67748a0051efd3c
Yevhenii <freshjs.fte.@gmail.com>
2022-01-27 16:45:40 +0200

    Add module example

--- assets/js/func.js
+++ assets/js/func.js
@@ -1,26 +1,14 @@
-function min(num1, num2) {
-  let bool = num1 <= num2;
+// напишите функцию которая вернет всегда положительную версию числа
+// (модуль числа)
 
-  if (bool) {
-    return num1;
-  } else {
-    return num2;
+function modul(num) {
+  if (num < 0) {
+    return num * -1;
   }
+
+  return num;
 }
 
-let result = min(-5, 4);
+const moduleChisla = modul(20);
 
-console.log(result);
-
-
-
-
-function minV2 (num1, num2) {
-  const result = Math.min(num1, num2);
-
-  return result
-}
-
-const res = minV2 (-5, 4)
-
-console.log(res)
+console.log(moduleChisla);
