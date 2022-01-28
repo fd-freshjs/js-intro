02e4cac8ea9d940945f830bfa51435cea785e80f
Yevhenii <freshjs.fte.@gmail.com>
2022-01-28 14:33:14 +0200

    Add ternary, || and && operators

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,58 @@
-const num1 = prompt()
-const operator = prompt()
-const num2 = prompt()
+const num1 = prompt();
+const operator = prompt();
+const num2 = prompt();
 
 function calculator(a, op, b) {
-  if(isNaN(a) || isNaN(b)) {
+
+  // ||
+  if (isNaN(a)) {
+    return null;
+  }
+  if (isNaN(b)) {
     return null;
   }
 
-  if (op !== '+' || op !== '-') {
-    return null;
+  // &&
+  if (isNaN(a)) {
+    if (isNaN(b)) {
+      return null;
+    }
   }
 
-  if (op === '+') {
-    return a + b;
-  }
+  switch (op) {
+    case "+":
+      return a + b;
 
-  if (op === '-') {
-    return a - b;
+    case "-":
+      return a - b;
+
+    case "*":
+      return a * b;
+
+    case "/":
+      return a / b;
+
+    case "%":
+      return a % b;
+
+    default:
+      return null;
   }
-  
-  return null;
 }
 
 calculator(num1, operator, num2);
 
 function sayHello(name) {
-  return 'Hello, ' + name;
+  return "Hello, " + name;
+}
+
+function max(a, b) {
+  if (a >= b) {
+    return a;
+  }
+  return b;
+}
+
+function min(a, b) {
+  return a >= b ? b : a;
 }
