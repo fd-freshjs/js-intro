0d57a33f04383efe46898c9e29b0a524300dec1f
Yevhenii <freshjs.fte.@gmail.com>
2022-01-31 15:19:04 +0200

    Add square equation solution

--- assets/js/tasks.js
+++ assets/js/tasks.js
@@ -40,6 +40,30 @@ function isTriangle(a, b, c) {
 // 1. функция которая приветствует пользователя в зависимости от полученного имени
 // sayHello('Viktor') // Привет Viktor
 
+const username = prompt('Как вас зовут?');
+
+function sayHello(name) {
+  alert("Привет " + name)
+}
+
+sayHello(username);
+
+/* 
+функция которая вернет правду если полученная параметр строка не пустая
+isNotEmpty('') false
+isNotEmpty('abc') true
+isNotEmpty(' ') true
+*/
+
+function isNotEmpty(str) {
+  if (str !== "") {
+    return true;
+  }
+  return false;
+}
+
+isNotEmpty(' ') // true
+
 // 2. функция которая вернет правду если полученная параметр строка не пустая
 // isNotEmpty('') // false
 // isNotEmpty('abc') // true
@@ -59,6 +83,41 @@ function isTriangle(a, b, c) {
 // * функция которая вернет модуль (abs) числа (если пришло отрицательное то вернет положительное, если положительное то вернет положительное)
 // * функция которая вернет случайное (random) число от 0 до 10
 // * функция которая вернет квадратный корень числа (sqrt)
+function sqrt(x) {
+  const result = Math.sqrt(x);
+  return result;
+}
+
+Math.random() * 10
+0.5 * 10 // 5
+0.1 * 10 // 1
+0.0001 * 10 // 0.001
+
+function solveSquareEquation(a, b, c) {
+
+  const D = b * b - 4 * a * c;
+
+  if (D > 0) {
+    const x1 = (-b + Math.sqrt(D))/(2*a);
+    const x2 = (-b - Math.sqrt(D))/(2*a);
+    return 'X1=' + x1 + ';X2=' + x2;
+  }
+
+  if (D === 0) {
+    const x = (-b + Math.sqrt(D))/(2*a)
+    return x;
+  }
+
+  if (D < 0) {
+    return NaN;
+  }
+}
+
+solveSquareEquation(4, 5, 6);
+solveSquareEquation(4, 5, 6);
+
+// 'X1=4;X2=-4'
+
 // num ** (1/2)
 // * функция которая посчитает корни квадратного уравнения (формула есть в интернете)
 // sqrt
