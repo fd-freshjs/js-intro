76be1d27d174c3861380185239a35da04a52f681
Yevhenii <freshjs.fte.@gmail.com>
2022-01-31 17:05:14 +0200

    Add for with examples

--- assets/js/cycles.js
+++ assets/js/cycles.js
@@ -2,13 +2,40 @@ function mult(a, b) {
   return a * b;
 }
 
-let iter = 0;
-console.log('До цикла', iter);
+// let iter = 0;
+// console.log('До цикла', iter);
 
-while (iter < 10) { // true тогда выполняем
+/* while (iter < 10) { // true тогда выполняем
   console.log(iter);
 
   iter ++;
+} */
+
+// console.log('После цикла',  iter);
+
+for (let iter = 1; iter < 10; iter++) {
+  console.log('Умножение на ' + iter);
+
+  for (let j = 1; j < 10; j++) {
+    console.log(iter, j); 
+  }
 }
 
-console.log('После цикла',  iter);
+
+/* 
+
+  for (let iter = 0; iter < 5; i++) {
+    if (iter % 2 === 0) {
+      continue;
+    }
+
+    log(iter) // 1, 3
+  }  
+
+  for (let iter = 1; iter < 5; i += 2) {
+
+    log(iter) // 1, 3
+  } 
+
+*/
+
