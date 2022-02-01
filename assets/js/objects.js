b42c8e242ffc423835237e04286b90e00693bc49
Yevhenii <freshjs.fte.@gmail.com>
2022-02-01 16:42:04 +0200

    Add object example

---
+++ assets/js/objects.js
@@ -0,0 +1,27 @@
+
+// пользователь
+// имя, фамилии, телефон, дата рождения, адрес, ел почта
+
+const userFirstname = 'Василий';
+const userLastname = 'Егоров';
+
+const userPhone = '380984637465';
+
+const userEmail = 'mail@gmail.com';
+
+Number('123')
+
+const obj = {
+  key: 234,
+  key2: 'string',
+}
+
+console.log(obj);
+
+if (obj.key3) {
+  console.log(obj.key3);
+}
+//null, NaN, undefined, 0
+else {
+  console.log('key3 = undefined');
+}
